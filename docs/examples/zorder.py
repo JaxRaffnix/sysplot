b349fb40d214c1675a877ab9d02d10c29ca43f52
Alice <alice@example.com>
2026-03-09 11:16:38 +0100

    added zorder example

---
+++ docs/examples/zorder.py
@@ -0,0 +1,13 @@
+"""
+Zorder Example
+=====================================
+
+Zorder controls the drawing order of plot elements. Elements with higher zorder values are drawn on top of those with lower values. This example demonstrates how to use zorder to control a plot call resulting betwenn gridlines and other plot calls.
+"""
+
+# TODO: show off different zorders.
+# find out which value is needed to plot after gridlines but before other plot calls
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
